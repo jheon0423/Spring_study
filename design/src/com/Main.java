0e8e7bff10b186cd3a9d081670a08e2143336b0e
jheon0423 <jh0423lee@naver.com>
2021-09-06 12:48:00 +0900

    hi

--- design/src/com/Main.java
+++ design/src/com/Main.java
@@ -1,5 +1,8 @@
 package design.src.com;
 
+import java.util.concurrent.atomic.AtomicLong;
+
+import design.src.com.aop.AopBrowser;
 //import design.src.com.proxy.Browser;
 import design.src.com.proxy.BrowserProxy;
 import design.src.com.proxy.IBrowser;
@@ -20,8 +23,26 @@ public class Main {
         browser.show();
         */
 
-        IBrowser browser = new BrowserProxy("www.naver.com");
-        browser.show();
+        // IBrowser browser = new BrowserProxy("www.naver.com");
+        // browser.show();
+        AtomicLong start = new AtomicLong();
+        AtomicLong end = new AtomicLong();
+        IBrowser aopBrowser = new AopBrowser("www.naver.com",
+                ()->{
+                    System.out.println("before");
+                    start.set(System.currentTimeMillis());
+                } ,
+                ()->{
+                    long now = System.currentTimeMillis();
+                    end.set(now-start.get());
+                } );
+
+        aopBrowser.show();
+        System.out.println("loading time : "+end.get());
+
+
+        aopBrowser.show();
+        System.out.println("loading time : "+end.get());
     }
 
     
